a7faee8d73cc29d9028e0216958ac3256fc37360
unknown <abdullasalimov7@gmail.com>
2022-04-03 19:49:41 +0500

    asdf

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -10,3 +12,5 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("thankyou/", views.thankyou, name="thankyou"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
